7f5eadef0064070cb565a18bc881789b29d53447
Fiona <petillon.fiona@gmail.com>
2020-05-25 19:24:57 +0200

    test

--- exo4.py
+++ exo4.py
@@ -1,11 +1,10 @@
 def listSum(l):
-     if len(l) == 0:
-         return 0
-     else:
-         return listSum(l[1:]) + l[0]
+    if len(l) == 0:
+        return 0
+    return listSum(l[1:]) + l[0]
 
 
-
-print(listSum([])) # 0
 print(listSum([42])) # 42
 print(listSum([3,1,5,2])) # 11
+
+
